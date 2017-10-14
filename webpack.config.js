3f4006701ac63fb1cc400aec88ba48255e692b81
johnnyb <johnnyb.ddd.com>
2017-10-14 16:34:26 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+	entry: './app/index.js',
+	output: {
+		path: path.resolve(__dirname,'dist'),
+		filename: '[name].bundle.js'
+	}
+
+}
